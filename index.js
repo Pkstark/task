9d33d6fe43890cf90fb62eb4eb0881343a9773d9
PraveenDevship <108656958+PraveenDevship@users.noreply.github.com>
2022-09-02 11:29:50 +0530

    change

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const mongoose = require('mongoose');
 const url = "mongodb+srv://client:client123@cluster0.jqa6rrt.mongodb.net/?retryWrites=true&w=majority"
 const cors = require("cors")
 const rout = require('./controller/route');
-require('dotenv').config();
-const PORT = process.env.PORT || 7000;
+const PORT = process.env.PORT || 7000
 
 mongoose.connect(url).then(()=>{
     console.log("Db Connected Suessfully")
